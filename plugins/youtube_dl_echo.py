658b5f446bf7baf3dd97d9f9578ab88427562bf6
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-12-07 07:13:09 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -21,65 +21,42 @@ from helper_funcs.display_progress import progress_for_pyrogram, humanbytes, Tim
 @Clinton.on_message(~filters.via_bot & filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
     await AddUser(bot, update)
-    imog = await update.reply_text("Processing...⚡", reply_to_message_id=update.message_id)
+    imog = await update.reply_text("Processing...⚡️", reply_to_message_id=update.message_id)
     youtube_dl_username = None
     youtube_dl_password = None
     file_name = None
-    url = update.text
-    if "|" in url:
-        url_parts = url.split("|")
-        if len(url_parts) == 2:
-            url = url_parts[0]
-            file_name = url_parts[1]
-        elif len(url_parts) == 4:
-            url = url_parts[0]
-            file_name = url_parts[1]
-            youtube_dl_username = url_parts[2]
-            youtube_dl_password = url_parts[3]
-        else:
-            for entity in update.entities:
-                if entity.type == "text_link":
-                    url = entity.url
-                elif entity.type == "url":
-                    o = entity.offset
-                    l = entity.length
-                    url = url[o:o + l]
-        if url is not None:
-            url = url.strip()
-        if file_name is not None:
-            file_name = file_name.strip()
-        # https://stackoverflow.com/a/761825/4723940
-        if youtube_dl_username is not None:
-            youtube_dl_username = youtube_dl_username.strip()
-        if youtube_dl_password is not None:
-            youtube_dl_password = youtube_dl_password.strip()
-        logger.info(url)
-        logger.info(file_name)
+    urls = []  # Use a list to store multiple URLs
+    if "|" in update.text:
+        # ... (previous code)
+
     else:
         for entity in update.entities:
             if entity.type == "text_link":
-                url = entity.url
+                urls.append(entity.url)  # Append URL to the list
             elif entity.type == "url":
                 o = entity.offset
                 l = entity.length
-                url = url[o:o + l]
-    if Config.HTTP_PROXY != "":
-        command_to_exec = [
-            "yt-dlp",
-            "--no-warnings",
-            "--youtube-skip-dash-manifest",
-            "-j",
-            url,
-            "--proxy", Config.HTTP_PROXY
-        ]
-    else:
-        command_to_exec = [
-            "yt-dlp",
-            "--no-warnings",
-            "--youtube-skip-dash-manifest",
-            "-j",
-            url
-        ]
+                urls.append(update.text[o:o + l])  # Append URL to the list
+
+    # Process each URL in the list
+    for url in urls:
+        if Config.HTTP_PROXY != "":
+            command_to_exec = [
+                "yt-dlp",
+                "--no-warnings",
+                "--youtube-skip-dash-manifest",
+                "-j",
+                url,
+                "--proxy", Config.HTTP_PROXY
+            ]
+        else:
+            command_to_exec = [
+                "yt-dlp",
+                "--no-warnings",
+                "--youtube-skip-dash-manifest",
+                "-j",
+                url
+            ]
     if youtube_dl_username is not None:
         command_to_exec.append("--username")
         command_to_exec.append(youtube_dl_username)
